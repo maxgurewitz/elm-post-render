90abd07b192c97ed1d22f2a8c0f4ef9e6b3b866c
maxgurewitz <maxthegeek1@gmail.com>
2016-03-04 00:16:15 -0800

    watching working

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,33 @@
 module Main where
 
 import Html exposing (..)
+import Html.Events exposing (onClick)
+import Effects as FX
+import StartApp
 
-main = text "foo"
+main = app.html
+
+app =
+  StartApp.start
+    { init = init
+    , update = update
+    , view = view
+    , inputs = []
+    }
+
+type alias Model = { count : Int }
+
+init = ({ count = 1 }, FX.none)
+
+view address model =
+  div
+    []
+    [ text (toString model.count)
+    , button [ onClick address Increment ] [ text "increment" ]
+    ]
+
+type Action = Increment
+
+update action model =
+  case action of
+    Increment -> ({ model | count = model.count + 1 }, FX.none)
