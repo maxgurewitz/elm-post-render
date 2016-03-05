ff23a501029cf7932dc0d26fd478e95d51d0add4
maxgurewitz <maxthegeek1@gmail.com>
2016-03-04 16:14:53 -0800

    wip

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,19 @@ module Main where
 
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
 import Effects as FX
+import List
+import Signal
 import StartApp
+import Debug
+
+
+
+import Task
+
+-- port setFocus : Signal Int
+-- port setFocus = Signal.map .renderedCount app.model
 
 main = app.html
 
@@ -15,19 +26,63 @@ app =
     , inputs = []
     }
 
-type alias Model = { count : Int }
+type alias Model =
+  { count : Int
+  , renderedCount : Int
+  }
 
-init = ({ count = 1 }, FX.none)
+onRenderIncrement = FX.tick
+  (\_ ->
+    let _ = Debug.log "loc1" "loc2"
+    in RenderedIncrement)
+
+init =
+  ({ count = 1
+  , renderedCount = 0
+  }
+  -- , onRenderIncrement
+  , incrementTask
+  )
+
+incrementTask = FX.task (Task.succeed Increment)
 
 view address model =
-  div
-    []
-    [ text (toString model.count)
-    , button [ onClick address Increment ] [ text "increment" ]
-    ]
+  let
+    _ = Debug.log "boop" "bam"
+    incrementButton = button [ onClick address Increment ] [ text "increment" ]
+    boxes =
+      List.repeat
+        model.count
+        (input
+          [ class "box" ]
+          []
+        )
+  in
+    div
+      []
+      (incrementButton :: boxes)
 
-type Action = Increment
+
+type Action = Increment | RenderedIncrement
 
 update action model =
   case action of
-    Increment -> ({ model | count = model.count + 1 }, FX.none)
+    Increment ->
+      let
+        _ = Debug.log "moop" "doop"
+      in
+        ({ model
+            | count = model.count + 1
+        }
+        , onRenderIncrement
+        )
+    RenderedIncrement ->
+      let
+        _ = Debug.log "foo" "bar"
+      in
+        ({ model
+            | renderedCount = model.count
+        }
+        , FX.none
+        )
+
